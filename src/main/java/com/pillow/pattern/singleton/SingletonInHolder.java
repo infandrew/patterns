330c0e494f22b40ce37bf097e535aa63e4d63a95
infandrew <infandrew@gmail.com>
2015-11-25 23:23:40 +0200

    Added first realization of Singlenton

--- src/main/java/com/pillow/pattern/singleton/SingletonInHolder.java
+++ src/main/java/com/pillow/pattern/singleton/SingletonInHolder.java
@@ -2,4 +2,16 @@ package com.pillow.pattern.singleton;
 
 public class SingletonInHolder {
 
+    private static class Holder {
+        static private SingletonInHolder instance = new SingletonInHolder();
+    }
+
+    private SingletonInHolder() {
+
+    }
+
+    public static SingletonInHolder getInstance() {
+        return Holder.instance;
+    }
+
 }
